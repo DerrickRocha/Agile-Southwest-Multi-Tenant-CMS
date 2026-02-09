f9b9e5c854cd4a448a85d2021a6d8cc002472281
derrick <drocha616@gmail.com>
2026-02-09 00:49:13 -0700

    Guaranteed loging on failure in RequestLoggingMiddleware.

--- AgileSouthwestCMSAPI/Middleware/RequestLoggingMiddleware.cs
+++ AgileSouthwestCMSAPI/Middleware/RequestLoggingMiddleware.cs
@@ -4,21 +4,25 @@ namespace AgileSouthwestCMSAPI.Middleware;
 
 public sealed class RequestLoggingMiddleware(ILogger<RequestLoggingMiddleware> logger) : IMiddleware
 {
-    public async Task InvokeAsync(
-        HttpContext context,
-        RequestDelegate next)
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         var sw = Stopwatch.StartNew();
 
-        await next(context);
+        try
+        {
+            await next(context);
+        }
+        finally
+        {
+            sw.Stop();
 
-        sw.Stop();
-
-        logger.LogInformation(
-            "HTTP {Method} {Path} responded {StatusCode} in {Elapsed}ms",
-            context.Request.Method,
-            context.Request.Path,
-            context.Response.StatusCode,
-            sw.ElapsedMilliseconds);
+            logger.LogInformation(
+                "HTTP {Method} {Path} responded {StatusCode} in {Elapsed}ms",
+                context.Request.Method,
+                context.Request.Path,
+                context.Response?.StatusCode,
+                sw.ElapsedMilliseconds);
+        }
     }
+
 }
