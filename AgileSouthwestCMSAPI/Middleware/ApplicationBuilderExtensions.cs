4d8b6aa7e2012839c5cc77ff10e8bf75d139db5f
derrick <drocha616@gmail.com>
2026-02-09 01:23:37 -0700

    Using title from error message in UseApiExceptionHandling.

--- AgileSouthwestCMSAPI/Middleware/ApplicationBuilderExtensions.cs
+++ AgileSouthwestCMSAPI/Middleware/ApplicationBuilderExtensions.cs
@@ -11,13 +11,21 @@ public static class ApplicationBuilderExtensions
         {
             errorApp.Run(async context =>
             {
+                if (context.Response.HasStarted)
+                {
+                    return;
+                }
+                
                 var env = context.RequestServices
                     .GetRequiredService<IHostEnvironment>();
+                
+                var exception = context.Features
+                    .Get<IExceptionHandlerFeature>()?
+                    .Error;
 
                 var feature = context.Features
                     .Get<IExceptionHandlerFeature>();
 
-                var exception = feature?.Error;
                 
                 var statusCode = exception switch
                 {
@@ -26,10 +34,18 @@ public static class ApplicationBuilderExtensions
                     KeyNotFoundException => StatusCodes.Status404NotFound,
                     _ => StatusCodes.Status500InternalServerError
                 };
+                
+                var title = statusCode switch
+                {
+                    StatusCodes.Status400BadRequest => "Bad request",
+                    StatusCodes.Status401Unauthorized => "Unauthorized",
+                    StatusCodes.Status404NotFound => "Resource not found",
+                    _ => "An unexpected error occurred"
+                };
 
                 var problem = new ProblemDetails
                 {
-                    Title = "An unexpected error occurred",
+                    Title = title,
                     Status = statusCode,
                     Type = $"https://httpstatuses.com/{statusCode}",
                     Instance = context.Request.Path
