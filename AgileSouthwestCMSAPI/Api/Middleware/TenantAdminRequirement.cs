aa50fa15bcb7723bbac6b0f532b5c9a13a91a647
derrick <drocha616@gmail.com>
2026-03-08 16:07:56 -0600

    Removed unused files.

--- AgileSouthwestCMSAPI/Api/Middleware/TenantAdminRequirement.cs
+++ AgileSouthwestCMSAPI/Api/Middleware/TenantAdminRequirement.cs
@@ -2,7 +2,4 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace AgileSouthwestCMSAPI.Api.Middleware;
 
-public class TenantAdminRequirement: IAuthorizationRequirement
-{
-    
-}
+public class TenantAdminRequirement: IAuthorizationRequirement;
