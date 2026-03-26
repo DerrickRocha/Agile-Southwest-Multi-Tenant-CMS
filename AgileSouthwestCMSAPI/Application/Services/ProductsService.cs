52018b5fd8c7016fed9b6fa9d663a5b9876aae51
derrick <drocha616@gmail.com>
2026-03-25 22:53:48 -0600

    Added ProductsServiceTests.

--- AgileSouthwestCMSAPI/Application/Services/ProductsService.cs
+++ AgileSouthwestCMSAPI/Application/Services/ProductsService.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AgileSouthwestCMSAPI.Application.Services;
 
-public class ProductsService(ITenantContext context, CmsDbContext database) : IProductsService
+public class ProductsService(ITenantContext context, CmsDbContext database, bool skipTransactionsForTesting = false) : IProductsService
 {
     public async Task<ProductResult> CreateProduct(CreateProductRequest request)
     {
@@ -17,17 +17,18 @@ public class ProductsService(ITenantContext context, CmsDbContext database) : IP
         var requiresBasePrice = request.ProductOptions.Any(o => o.ProductOptionChoices.Any(c => c.PriceDelta == 0));
         if (requiresBasePrice && request.BasePrice <= 0) throw new InvalidOperationException("Base price must be greater than 0");
 
+        if (skipTransactionsForTesting) return await WriteProduct(request, tenant.Id);
+        
         return await strategy.ExecuteAsync(async () =>
         {
             await using var transaction = await database.Database.BeginTransactionAsync();
 
             try
             {
-                var product = request.ToProduct(tenant.Id);
-                database.Products.Add(product);
-                await database.SaveChangesAsync();
+
+                var result = await WriteProduct(request, tenant.Id);
                 await transaction.CommitAsync();
-                return product.ToProductResult();
+                return result;
             }
             catch
             {
@@ -35,6 +36,14 @@ public class ProductsService(ITenantContext context, CmsDbContext database) : IP
                 throw;
             }
         });
+    }
+
+    private async Task<ProductResult> WriteProduct(CreateProductRequest request, int tenantId)
+    {
+        var product = request.ToProduct(tenantId);
+        database.Products.Add(product);
+        await database.SaveChangesAsync();
+        return product.ToProductResult();
     }
 
     public async Task<ProductResult> GetProduct(int id)
