6f97af1ae5557e5a87682b3811a624e97203d784
derrick <drocha616@gmail.com>
2026-04-01 22:54:22 -0600

    Added ProductsControllerTests.

--- Tests/Api/Controllers/ProductsControllerTests.cs
+++ Tests/Api/Controllers/ProductsControllerTests.cs
@@ -2,11 +2,10 @@ using AgileSouthwestCMSAPI.Api.Controllers;
 using AgileSouthwestCMSAPI.Api.Requests.Products;
 using AgileSouthwestCMSAPI.Application.DTOs.Products;
 using AgileSouthwestCMSAPI.Application.Interfaces;
-using AgileSouthwestCMSAPI.Application.Services;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
-using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using Assert = Xunit.Assert;
 
 namespace Tests.Api.Controllers;
 
@@ -17,12 +16,68 @@ public class ProductsControllerTests
     private ProductsController CreateController() => new(_service.Object);
     
     [Fact]
-    public async Task Create_ReturnsOk_With_Product()
+    public async Task GetAll_ReturnsOk_WithPagedResults()
+    {
+        var query = new GetProductsQuery(Page: 1, PageSize: 20);
+        var result = new PagedResult<ProductListItemResult>(
+            [
+                new ProductListItemResult(1, "Coffee", 1000, true),
+                new ProductListItemResult(2, "Tea", 700, true)
+            ],
+            1,
+            20,
+            2);
+
+        _service.Setup(s => s.GetProducts(query)).ReturnsAsync(result);
+
+        var controller = CreateController();
+
+        var response = await controller.GetAll(query);
+
+        var okResult = Assert.IsType<OkObjectResult>(response);
+        Assert.Equal(result, okResult.Value);
+
+        _service.Verify(s => s.GetProducts(query), Times.Once);
+    }
+
+    [Fact]
+    public async Task Patch_ReturnsOk_WithPatchedProduct()
+    {
+        var request = new PatchProductRequest(
+            Name: "Updated Name",
+            Description: "Updated Description",
+            BasePrice: 1500,
+            IsActive: true,
+            Options: null);
+
+        var result = new ProductResult
+        {
+            Id = 10,
+            Name = "Updated Name",
+            Description = "Updated Description",
+            BasePrice = 1500,
+            IsActive = true
+        };
+
+        _service.Setup(s => s.PatchProduct(10, request)).ReturnsAsync(result);
+
+        var controller = CreateController();
+
+        var response = await controller.Patch(10, request);
+
+        var okResult = Assert.IsType<OkObjectResult>(response);
+        Assert.Equal(result, okResult.Value);
+
+        _service.Verify(s => s.PatchProduct(10, request), Times.Once);
+    }
+
+    [Fact]
+    public async Task Create_ReturnsCreatedAtAction_WithProduct()
     {
         var request = new ProductRequest
         {
-            Name = "Test Product",
-            Description = "Test Description",
+            Name = "Coffee",
+            Description = "Fresh coffee",
             BasePrice = 1000,
             IsActive = true,
             Options = []
@@ -31,21 +86,80 @@ public class ProductsControllerTests
         var result = new ProductResult
         {
             Id = 42,
-            Name = "Test Product",
-            Description = "Test Description",
+            Name = "Coffee",
+            Description = "Fresh coffee",
             BasePrice = 1000,
             IsActive = true
         };
-        
+
         _service.Setup(s => s.CreateProduct(request)).ReturnsAsync(result);
+
         var controller = CreateController();
-        
+
         var response = await controller.Create(request);
-        var createdResult = Assert.IsInstanceOfType<CreatedAtActionResult>(response);
-        Assert.AreEqual(nameof(ProductsController.Get), createdResult.ActionName);
-        Assert.AreEqual(result, createdResult.Value);
-        
+
+        var createdResult = Assert.IsType<CreatedAtActionResult>(response);
+        Assert.Equal(nameof(ProductsController.Get), createdResult.ActionName);
+        Assert.Equal(result.Id, createdResult.RouteValues!["id"]);
+        Assert.Equal(result, createdResult.Value);
 
         _service.Verify(s => s.CreateProduct(request), Times.Once);
+    }
+
+    [Fact]
+    public async Task Get_ReturnsOk_WithProduct()
+    {
+        var result = new ProductResult
+        {
+            Id = 5,
+            Name = "Coffee",
+            Description = "Fresh coffee",
+            BasePrice = 1000,
+            IsActive = true
+        };
+
+        _service.Setup(s => s.GetProduct(5)).ReturnsAsync(result);
+
+        var controller = CreateController();
+
+        var response = await controller.Get(5);
+
+        var okResult = Assert.IsType<OkObjectResult>(response);
+        Assert.Equal(result, okResult.Value);
+
+        _service.Verify(s => s.GetProduct(5), Times.Once);
+    }
+
+    [Fact]
+    public async Task Update_ReturnsOk_WithUpdatedProduct()
+    {
+        var request = new ProductRequest
+        {
+            Name = "Updated Coffee",
+            Description = "Updated Description",
+            BasePrice = 1200,
+            IsActive = true,
+            Options = []
+        };
+
+        var result = new ProductResult
+        {
+            Id = 5,
+            Name = "Updated Coffee",
+            Description = "Updated Description",
+            BasePrice = 1200,
+            IsActive = true
+        };
+
+        _service.Setup(s => s.UpdateProduct(5, request)).ReturnsAsync(result);
+
+        var controller = CreateController();
+
+        var response = await controller.Update(5, request);
+
+        var okResult = Assert.IsType<OkObjectResult>(response);
+        Assert.Equal(result, okResult.Value);
+
+        _service.Verify(s => s.UpdateProduct(5, request), Times.Once);
     }
 }
