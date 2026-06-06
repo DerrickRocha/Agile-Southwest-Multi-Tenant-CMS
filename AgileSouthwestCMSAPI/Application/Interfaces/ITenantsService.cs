7eebecf9da8059046c7710f5cbff62687412b74f
derrick <drocha616@gmail.com>
2026-06-05 22:39:08 -0600

    Added get all tenants for user.

--- AgileSouthwestCMSAPI/Application/Interfaces/ITenantsService.cs
+++ AgileSouthwestCMSAPI/Application/Interfaces/ITenantsService.cs
@@ -1,3 +1,4 @@
+using AgileSouthwestCMSAPI.Application.DTOs.Products;
 using AgileSouthwestCMSAPI.Application.DTOs.Tenants;
 
 namespace AgileSouthwestCMSAPI.Application.Interfaces;
@@ -6,6 +7,7 @@ public interface ITenantsService
 {
     Task<AddTenantResult> AddTenant(AddTenantRequest request);
     Task<GetTenantResult> GetTenant();
+    Task<GetTenantResult[]> GetAllTenantsForAdmin();
     Task<UpdateTenantResult> UpdateTenant(UpdateTenantRequest request);
     Task<GetTenantSubscriptionResult> GetTenantSubscription(GetTenantSubscriptionRequest request);
     Task<ChangeTenantSubscriptionResult> UpdateTenantSubscription(ChangeTenantSubsciptionRequest request);
